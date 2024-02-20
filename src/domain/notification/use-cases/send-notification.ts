31c474340c988f10472be1c86289d39957876fc4
luminuszz <luminuszz43@gmail.com>
2024-02-20 18:36:37 -0300

    add new queue for get unread for scrapping

--- src/domain/notification/use-cases/send-notification.ts
+++ src/domain/notification/use-cases/send-notification.ts
@@ -1,27 +1,41 @@
-import { Either, right } from '@core/either';
+import { Either, left, right } from '@core/either';
 import { UseCaseImplementation } from '@core/use-case';
 import { Injectable } from '@nestjs/common';
 import { InvalidNotificationOperation } from '../errors/invalid-notification-operation';
 import { NotificationRepository } from '../notification.repository';
 import { Notification } from '../notifications';
+import { FindSubscriberByRecipientId } from '@domain/subscriber/use-cases/find-subscriber-by-recipient-id';
+import { ResourceNotFound } from '@domain/subscriber/use-cases/errors/resource-not-found';
 
 interface SendNotificationUseCaseProps {
   content: string;
   recipientId: string;
 }
 
-type SendNotificationUseCaseResponse = Either<InvalidNotificationOperation, { notification: Notification }>;
+type SendNotificationUseCaseResponse = Either<
+  InvalidNotificationOperation | ResourceNotFound,
+  { notification: Notification }
+>;
 
 @Injectable()
 export class SendNotificationUseCase
   implements UseCaseImplementation<SendNotificationUseCaseProps, SendNotificationUseCaseResponse>
 {
-  constructor(private readonly notificationRepo: NotificationRepository) {}
+  constructor(
+    private readonly notificationRepo: NotificationRepository,
+    private readonly findSubscriberByRecipientId: FindSubscriberByRecipientId,
+  ) {}
 
   async execute({ content, recipientId }: SendNotificationUseCaseProps): Promise<SendNotificationUseCaseResponse> {
+    const results = await this.findSubscriberByRecipientId.execute({ recipientId: recipientId });
+
+    if (results.isLeft()) return left(results.value);
+
+    const { subscriber } = results.value;
+
     const notification = Notification.create({
       content,
-      recipientId,
+      recipientId: subscriber.id,
       createdAt: new Date(),
     });
 
