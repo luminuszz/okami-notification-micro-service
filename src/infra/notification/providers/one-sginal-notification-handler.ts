07da258a15b5b294dc10aba21df7300cce78a1a9
luminuszz <luminuszz43@gmail.com>
2024-02-21 13:42:00 -0300

    fix payload

--- src/infra/notification/providers/one-sginal-notification-handler.ts
+++ src/infra/notification/providers/one-sginal-notification-handler.ts
@@ -1,7 +1,7 @@
 import { EnvService } from '@app/infra/env/env.service';
 import { NotificationPublisherPayload } from '@domain/notification/notification-publisher';
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { NotificationEventEmitter } from '../notification-event-emitter';
 import { NotificationContentParsed } from './dto/notification-parsed.dto';
@@ -14,17 +14,21 @@ export class OneSignalNotificationPublisher {
     private readonly httpService: HttpService,
   ) {}
 
+  private logger = new Logger(OneSignalNotificationPublisher.name);
+
   @OnEvent('notification.created')
   public async publish({ notification, subscriber }: NotificationPublisherPayload): Promise<void> {
     const content = JSON.parse(notification.content) as NotificationContentParsed;
 
     if (!subscriber.webPushSubscriptions?.length) return;
 
+    this.logger.log(
+      `Sending notification to ${subscriber.mobilePushSubscriptions?.map((item) => item.subscriberId).join(', ')}`,
+    );
+
     this.httpService.post('notifications', {
       app_id: this.envService.get('ONE_SIGNAL_APP_ID'),
-      include_aliases: {
-        include_subscription_ids: subscriber.webPushSubscriptions?.map((item) => item.subscriberId),
-      },
+      include_subscription_ids: subscriber.webPushSubscriptions?.map((item) => item.subscriberId),
       contents: {
         en: content.message,
       },
