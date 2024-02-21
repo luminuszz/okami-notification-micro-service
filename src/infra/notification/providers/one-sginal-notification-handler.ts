6f0875e6f84347a7632f0804b63f535c1b30fcb1
luminuszz <luminuszz43@gmail.com>
2024-02-21 14:25:08 -0300

    fix mobile push subscrier handler

--- src/infra/notification/providers/one-sginal-notification-handler.ts
+++ src/infra/notification/providers/one-sginal-notification-handler.ts
@@ -5,6 +5,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { NotificationEventEmitter } from '../notification-event-emitter';
 import { NotificationContentParsed } from './dto/notification-parsed.dto';
+import { map } from 'lodash';
 
 @Injectable()
 export class OneSignalNotificationPublisher {
@@ -20,15 +21,15 @@ export class OneSignalNotificationPublisher {
   public async publish({ notification, subscriber }: NotificationPublisherPayload): Promise<void> {
     const content = JSON.parse(notification.content) as NotificationContentParsed;
 
-    if (!subscriber.webPushSubscriptions?.length) return;
+    if (!subscriber.mobilePushSubscriptions?.length) return;
 
-    this.logger.log(
-      `Sending notification to ${subscriber.mobilePushSubscriptions?.map((item) => item.subscriberId).join(', ')}`,
-    );
+    const subscribersTokens = map(subscriber.mobilePushSubscriptions, 'subscriptionToken');
+
+    this.logger.log(`Sending notification to ${subscribersTokens.join(', ')}`);
 
     this.httpService.post('notifications', {
       app_id: this.envService.get('ONE_SIGNAL_APP_ID'),
-      include_subscription_ids: subscriber.webPushSubscriptions?.map((item) => item.subscriberId),
+      include_subscription_ids: subscribersTokens,
       contents: {
         en: content.message,
       },
